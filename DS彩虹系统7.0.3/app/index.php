8a07b06ff1c8cb6ca640da1b1d98119d569fc847
xiaojiu <120791773+xiaojiuaini@users.noreply.github.com>
2024-01-13 16:38:27 +0800

    Add files via upload

---
+++ DS彩虹系统7.0.3/app/index.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>代刷网-APP下载</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+			<header id="header">
+				<div class="content">
+					<h1><a href="https://dd88.lanzoux.com/iADCw0fsrlob">代刷网</a></h1>
+					<p>你也可以直接在线访问 <a href="https://dd88.lanzoux.com/iADCw0fsrlob">蓝奏云下载</a></p>
+					<ul class="actions">
+						<li><a href="https://dd88.lanzoux.com/iADCw0fsrlob" class="button primary icon fa-download">APP下载</a></li>
+						<li><a href="#one" class="button icon fa-chevron-down scrolly">APP介绍</a></li>
+					</ul>
+				</div>
+				<div class="image phone"><div class="inner"><img src="images/dg1.jpg" alt="" /></div></div>
+			</header>
+			<section id="one" class="wrapper style2 special">
+				<header class="major">
+					<h2>代刷网APP介绍</h2>
+					<img width="100%" src="images/jh3.jpg" />
+				</header>
+			</section>
+			<section id="two" class="wrapper">
+				<div class="inner alt">
+					<section class="spotlight">
+						<div class="image"><img src="images/js1.png" alt="" /></div>
+						<div class="content">
+							<h3>高效</h3>
+							<p>软件下载仅需几MB，节省了大量因打开网站所浪费的流量，享受更快的体验！</p>
+						</div>
+					</section>
+					
+					<section class="spotlight">
+						<div class="image"><img src="images/js2.png" alt="" /></div>
+						<div class="content">
+							<h3>快捷</h3>
+							<p>无需再从浏览器打开，下单、查单等功能样样具备！</p>
+						</div>
+					</section>
+					
+					<section class="spotlight">
+						<div class="image"><img src="images/js3.png" alt="" /></div>
+						<div class="content">
+							<h3>安全</h3>
+							<p>由站长原创，绝无盗号风险，值得信赖！</p>
+						</div>
+					</section>
+					
+				<section id="three" class="wrapper style2 special">
+				<header class="major">
+					<h2>加入狗子货源平台，开通分站享受超低价格
+</h2>
+				</header>
+				<ul class="actions special">
+				<li><a href="https://jq.qq.com/?_wv=1027&k=6BFamvoP" class="button">官方交流群</a></li>
+   		
+				</ul>
+			</section>
+
+			
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
